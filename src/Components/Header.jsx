71e307a085c2409a5b2a6e190d344c2def1e0ae5
unknown <kiantpetersen@gmail.com>
2023-09-12 12:32:06 +0200

    adding cart design

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import userImg from '../images/image-avatar.png'
+import productImg1 from '../images/image-product-1.jpg'
+
 function Header(props) {
     const [visibility, setVisibility] = useState(false)
+    const [cartItems, setCartItems] = useState([])
     function handleClick(e) {
         const cartContainer = document.querySelector('.cart-container')
 
@@ -14,6 +17,31 @@ function Header(props) {
             setVisibility(false)
 
         }
+    }
+    function getItem() {
+        return <div className='cart-item-container'>
+            <div className='cart-img-container'>
+                <img src={productImg1} className='cart-item-img' alt='Item_image' />
+            </div>
+            <div className='cart-item-textbox'>
+                <p className='item-name'>Fall Limited Edition Sneackers</p>
+                <p className='item-price'>$125.00 x 3 <span className='total-price'>$420.69</span></p>
+            </div>
+            <div className='cart-delete-icon'>
+                <ion-icon name="trash-outline"></ion-icon>
+            </div>
+        </div>
+    }
+    function getCartItems() {
+        return (
+            <div className='cart-item-list'>
+                {cartItems.map((item, key) => {
+                    return getItem()
+                })}
+
+                <button className='cart-checkout-btn'>Checkout</button>
+            </div>)
+
     }
     return (
         <header className='sneaker-nav'>
@@ -38,9 +66,11 @@ function Header(props) {
             </div>
             <div className='cart-container'>
                 <h2 className='cart-heading'>Cart</h2>
-                <div className='cart-item-list'>
-                    <p className='cart-default-text'>your cart is empty</p>
-                </div>
+                {/* <div className='cart-item-list'>
+                    {getCartItems()} */}
+                {cartItems.length > 0 ? getCartItems() : <p className='cart-default-text'>your cart is empty</p>}
+                {/* <p className='cart-default-text'>your cart is empty</p> */}
+                {/* </div> */}
 
             </div>
 
